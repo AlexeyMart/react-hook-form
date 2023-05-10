fd9d05003a2be2f8fe991d67640b5302c5ba7d90
AlexeyMart <alexey.martyshev@mail.ru>
2023-05-10 14:10:27 +0500

    rhk package, useForm, register, devtools (control)

--- src/components/YouTubeForm/YouTubeForm.tsx
+++ src/components/YouTubeForm/YouTubeForm.tsx
@@ -1,25 +1,43 @@
 import { FC } from "react";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 import "./YouTubeForm.css";
 
 export const YouTubeForm: FC = () => {
+  const form = useForm();
+  const { register, control } = form;
+  // const { name, ref, onChange, onBlur } = register("username");
+
   return (
-    <form className="YoutubeForm">
-      <label>
-        <p>Username</p>
-        <input type="text" id="username" name="username" />
-      </label>
+    <>
+      <form className="YoutubeForm">
+        <label>
+          <p>Username</p>
+          <input
+            type="text"
+            id="username"
+            // name={name}
+            // ref={ref}
+            // onChange={onChange}
+            // onBlur={onBlur}
+            {...register("username")}
+          />
+        </label>
 
-      <label>
-        <p>Email</p>
-        <input type="email" id="email" name="email" />
-      </label>
+        <label>
+          <p>Email</p>
+          <input type="email" id="email" {...register("email")} />
+        </label>
 
-      <label>
-        <p>Channel</p>
-        <input type="text" id="channel" name="channel" />
-      </label>
+        <label>
+          <p>Channel</p>
+          <input type="text" id="channel" {...register("channel")} />
+        </label>
 
-      <button>Submit</button>
-    </form>
+        <button>Submit</button>
+      </form>
+
+      <DevTool control={control} />
+    </>
   );
 };
